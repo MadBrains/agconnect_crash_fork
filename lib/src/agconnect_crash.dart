7ff487301a846951f3456ad540d105dc3b9a1849
radroot <radroot@gmail.com>
2022-12-22 13:16:20 +0400

    feat: null safety

--- lib/src/agconnect_crash.dart
+++ lib/src/agconnect_crash.dart
@@ -1,21 +1,5 @@
-/*
- * Copyright 2020. Huawei Technologies Co., Ltd. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import 'dart:async';
-
+// ignore_for_file: avoid_print, avoid_positional_boolean_parameters
+/// agconnect_crash: 1.3.0+300 null safety ext
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
@@ -24,7 +8,7 @@ enum LogLevel { debug, info, warning, error }
 
 /// 崩溃收集服务入口类
 class AGCCrash {
-  static const MethodChannel _channel = const MethodChannel('com.huawei.flutter/agconnect_crash');
+  static const MethodChannel _channel = MethodChannel('com.huawei.flutter/agconnect_crash');
 
   /// 获取AGCCrash实例
   static final AGCCrash instance = AGCCrash();
@@ -34,14 +18,14 @@ class AGCCrash {
     recordError(details.exceptionAsString(), details.stack);
   }
 
-  Future<void> recordError(dynamic exception, StackTrace stack, { bool fatal = false}) async {
+  Future<void> recordError(dynamic exception, StackTrace? _stack, {bool fatal = false}) async {
     assert(exception != null);
-    stack ??= StackTrace.current ?? StackTrace.fromString('');
+    final StackTrace stack = _stack ?? StackTrace.current;
     print('Error caught by AGCCrash : ${exception.toString()} \n${stack.toString()}');
-    await _channel.invokeMethod('recordError', <String, String>{
+    await _channel.invokeMethod<void>('recordError', <String, String>{
       'reason': exception.toString(),
-      'stack':stack.toString(),
-      'fatal':fatal.toString(),
+      'stack': stack.toString(),
+      'fatal': fatal.toString(),
     });
     return;
   }
@@ -61,7 +45,6 @@ class AGCCrash {
 
   /// 设置自定义用户标识符
   Future<void> setUserId(String userId) {
-    assert(userId != null);
     return _channel.invokeMethod('setUserId', <String, String>{
       'userId': userId,
     });
@@ -69,18 +52,11 @@ class AGCCrash {
 
   /// 添加自定义键值对
   Future<void> setCustomKey(String key, dynamic value) {
-    assert(key != null);
-    return _channel.invokeMethod('setCustomKey', <String, String>{
-      'key': key,
-      'value': value.toString()
-    });
+    return _channel.invokeMethod('setCustomKey', <String, String>{'key': key, 'value': value.toString()});
   }
 
   /// 添加自定义日志
-  Future<void> log(
-      {LogLevel level = LogLevel.info, @required String message}) {
-    assert(message != null);
-    return _channel.invokeMethod(
-        'customLog', <String, dynamic>{'level': level.index, 'message': message});
+  Future<void> log({required String message, LogLevel level = LogLevel.info}) {
+    return _channel.invokeMethod('customLog', <String, dynamic>{'level': level.index, 'message': message});
   }
 }
